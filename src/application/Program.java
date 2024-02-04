bc84cef470c629cb8ea0b88442809d4fa9cf2bc6
vinicinrn <zviniciusnascimento@gmail.com>
2024-02-04 08:39:13 -0300

    "Class: Position"

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Initial commit");
+
+        Position pos = new Position(3,5);
+        System.out.println(pos);
+
     }
 }
