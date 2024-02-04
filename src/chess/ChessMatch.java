d8b9eb9fc73ee4e3192ab89accc32126d97b861d
vinicinrn <zviniciusnascimento@gmail.com>
2024-02-04 18:17:52 -0300

    "Switching player"

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,11 +8,21 @@ import chess.pieces.Rook;
 import chess.pieces.King;
 
 public class ChessMatch {
+    private int turn;
+    private Color currentPlayer;
     private Board board;
 
     public ChessMatch(){
         board = new Board(8,8);
+        turn = 1;
+        currentPlayer = Color.WHITE;
         initialSetup();
+    }
+    public int getTurn(){
+        return turn;
+    }
+    public Color getCurrentPlayer(){
+        return currentPlayer;
     }
     public ChessPiece[][] getPieces(){
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -34,6 +44,7 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
+        nextTurn();
         return (ChessPiece)capturedPiece ;
     }
     private Piece makeMove (Position source, Position target){
@@ -46,6 +57,9 @@ public class ChessMatch {
         if (!board.thereIsAPiece(position)){
             throw new ChessException("Não existe peça na posição de origem");
         }
+        if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()){
+            throw new ChessException("A peça escolhida não é sua!");
+        }
         if (!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("Não existe movimentos possíveis para a peça escolhida");
         }
@@ -53,6 +67,14 @@ public class ChessMatch {
     private void validateTargetPosition(Position source, Position target){
         if (!board.piece(source).possibleMove(target)){
             throw new ChessException("A peça escolhida não pode mover para a posição de destino");
+        }
+    }
+    private void nextTurn(){
+        turn ++;
+        if(currentPlayer == Color.WHITE){
+            currentPlayer = Color.BLACK;
+        } else {
+            currentPlayer = Color.WHITE;
         }
     }
     private void placeNewPiece(char column, int row, ChessPiece piece){
