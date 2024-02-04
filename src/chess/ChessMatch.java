88944d4251c0b45ed760d4dfb44d51ac061b0d8c
vinicinrn <zviniciusnascimento@gmail.com>
2024-02-04 14:29:12 -0300

    "Possible moves for Rook"

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,6 +27,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece)capturedPiece ;
     }
@@ -42,6 +43,11 @@ public class ChessMatch {
         }
         if (!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("Não existe movimentos possíveis para a peça escolhida");
+        }
+    }
+    private void validateTargetPosition(Position source, Position target){
+        if (!board.piece(source).possibleMove(target)){
+            throw new ChessException("A peça escolhida não pode mover para a posição de destino");
         }
     }
     private void placeNewPiece(char column, int row, ChessPiece piece){
